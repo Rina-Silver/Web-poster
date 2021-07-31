4c4a846e83bfc380400579166f0426d9da409f6f
Унжакова Екатерина <katerina.unz@gmail.com>
2021-07-31 19:06:47 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+﻿let nav_items = Array.from(document.querySelectorAll('.nav-site__item'));
+let map__labels = Array.from(document.querySelectorAll('.map__label'));
+
+const clickOnNav = function() {
+    
+    map__labels.forEach(e => {
+        e.classList.remove('map__label_active');
+    });
+
+
+    let type = this.dataset.type;
+
+    
+    let type_labels = map__labels.filter(e => {
+        if (e.dataset.type.indexOf(type) != -1) {
+            return true;
+        }
+        return false;
+    });
+    
+    
+    if (!this.classList.contains('nav-site__item_active')) {
+        type_labels.forEach(e => {
+            e.classList.add('map__label_active');
+        });
+        this.classList.add('nav-site__item_active');
+    } else {
+        this.classList.remove('nav-site__item_active');
+    }
+};
+
+nav_items.forEach(e => {
+    e.addEventListener('click', clickOnNav);
+});
