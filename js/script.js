399e42305db1a1c4c567a512874631dfae96c844
Унжакова Екатерина <katerina.unz@gmail.com>
2021-08-13 01:24:18 +0700

    Рефакторинг кода #1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 ﻿let nav__items = Array.from(document.querySelectorAll('.nav-site__item'));
 let map__labels = Array.from(document.querySelectorAll('.map__label'));
 
-const clickOnNav = function () {
+const clickOnNav = function (event) {
   nav__items.forEach(e => {
-    if (e !== this) e.classList.remove('nav-site__item--active');
+    if (e !== event.currentTarget) e.classList.remove('nav-site__item--active');
   });
   map__labels.forEach(e => {
     e.classList.remove('map__label--active');
   });
 
-  let type = this.dataset.type;
+  let type = event.currentTarget.dataset.type;
 
   let type_labels = map__labels.filter(e => {
     if (e.dataset.type.indexOf(type) != -1) {
@@ -18,16 +18,15 @@ const clickOnNav = function () {
     return false;
   });
 
-  if (!this.classList.contains('nav-site__item--active')) {
+  if (!event.currentTarget.classList.contains('nav-site__item--active')) {
     type_labels.forEach(e => {
       e.classList.add('map__label--active');
     });
-    this.classList.add('nav-site__item--active');
+    event.currentTarget.classList.add('nav-site__item--active');
   } else {
-    this.classList.remove('nav-site__item--active');
+    event.currentTarget.classList.remove('nav-site__item--active');
   }
 };
-
 nav__items.forEach(e => {
   e.addEventListener('click', clickOnNav);
 });
